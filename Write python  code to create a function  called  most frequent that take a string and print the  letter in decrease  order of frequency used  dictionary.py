1e23e7df11fb20a77f7b79b1981564e6b2c1e3a6
Ganesh3715 <89771306+Ganesh3715@users.noreply.github.com>
2021-09-01 20:35:49 +0530

    Create Write python  code to create a function  called  most frequent that take a string and print the  letter in decrease  order of frequency used  dictionary.py

---
+++ Write python  code to create a function  called  most frequent that take a string and print the  letter in decrease  order of frequency used  dictionary.py
@@ -0,0 +1,13 @@
+
+Imort operation 
+d = dict ( )
+def most _ frequent ( )
+       Str1= input (" Enter  a String:  ")
+for i in str1: 
+         if I in d:
+            d[i]    +=1
+     else:
+          d[i] =1
+Most _frequent()
+des = dict (sorted (d.items (), key =operator .item getter (1),reverse =true) )
+Print (des)
